88859f3471bd599a04bf12bcbda693e8f2af66b2
SandeepMandlik <sandeepmandlik033@gmail.com>
2019-07-14 11:59:21 +0530

    changes by dev2

--- src/Package1/Login.java
+++ src/Package1/Login.java
@@ -4,6 +4,8 @@ public class Login {
 public static void main(String[] args) {
 	System.out.println("Welcome to Facebook");// first commit
 	System.out.println("Hello how are you"); //second change upload to git
+	
+	System.out.println("changes by devloper2"); // dev2 changes
 }
 	
 }
