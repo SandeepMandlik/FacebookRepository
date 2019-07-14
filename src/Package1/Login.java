c74992c38c4eef804032e8b17ffba59915b5cd59
SandeepMandlik <sandeepmandlik033@gmail.com>
2019-07-14 11:34:48 +0530

    Second commit by dev1

--- src/Package1/Login.java
+++ src/Package1/Login.java
@@ -2,7 +2,8 @@ package Package1;
 
 public class Login {
 public static void main(String[] args) {
-	System.out.println("Welcome to Facebook");
+	System.out.println("Welcome to Facebook");// first commit
+	System.out.println("Hello how are you"); //second change upload to git
 }
 	
 }
