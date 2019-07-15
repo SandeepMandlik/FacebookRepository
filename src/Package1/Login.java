502a25826c21a790241fc170ba44d5dcc32aebbd
SandeepMandlik <sandeepmandlik033@gmail.com>
2019-07-15 14:13:10 +0530

    new changes with white space

--- src/Package1/Login.java
+++ src/Package1/Login.java
@@ -7,6 +7,10 @@ public static void main(String[] args) {
 	
 	System.out.println("changes by devloper2"); // dev2 changes
 	
+	
+	
+	
+	
 	//import poject in new workspace by dev3
 	System.out.println("new changes in anather workspace");
 }
